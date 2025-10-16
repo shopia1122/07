a1826c2b5f6726294e310043b2f5b47308f5616d
shopia1122 <shopia123@sookmyung.ac.kr>
2025-10-16 16:15:20 +0900

    6-2

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int all_files;
-
+static int all_files;       //여기에 static 붙이면 undefined reference to 'all_files'라고 뜬다. 원래 all_files가 여기서 가져가는 거였는데 static 붙이는 순간  
+                            // all_files는 이 파일에서만 쓸 수 있는 변수가 돼버리므로  
+                           
+                           
+                           
 extern void sub();   //외부의 sub함수를 쓰겠다는 뜻, sub 함수 호출.  
 
 int main(int argc, char *argv[])
---
+++ sub.c
@@ -0,0 +1,9 @@
+
+
+extern int all_files;
+void sub(void)
+{
+     all_files = 10;  //여기서는 선언이 안되어 있음. 왜? main 함수에 전역변수로 선언되어 있으므로 
+     
+     
+     }
